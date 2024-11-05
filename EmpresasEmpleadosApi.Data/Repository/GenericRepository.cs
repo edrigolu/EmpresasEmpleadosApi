0a7ae95aa41e0dce5f790c7f10f9618594978761
Edwin Ricardo Gonzalez Luque <edwinricardo.gonzalezluque@hotmail.com>
2024-11-05 11:39:19 -0500

    Se crea solucion,proyectos y realaciones de dependencia para cada uno.

---
+++ EmpresasEmpleadosApi.Data/Repository/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using EmpresasEmpleadosApi.Data.Repository.Interface;
+using System.Linq.Expressions;
+
+namespace EmpresasEmpleadosApi.Data.Repository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        public GenericRepository()
+        {
+        }
+
+        public Task<IQueryable<TEntity>> Consult(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Edit(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Obtain(Expression<Func<TEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmpresasEmpleadosApi.Data/Repository/Interface/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace EmpresasEmpleadosApi.Data.Repository.Interface
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> Obtain(Expression<Func<TEntity, bool>> filter);
+        Task<TEntity> Create(TEntity entity);
+        Task<bool> Edit(TEntity entity);
+        Task<bool> Delete(TEntity entity);
+        Task<IQueryable<TEntity>> Consult(Expression<Func<TEntity, bool>> filter = null!);
+    }
+}
